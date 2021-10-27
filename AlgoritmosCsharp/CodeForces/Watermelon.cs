37802938cd583dc59dc8833771b0d619b3f3550e
Jonathan Maia <jtauan10@gmail.com>
2021-10-27 19:37:32 -0300

    Watermelon by CodeForces

---
+++ AlgoritmosCsharp/CodeForces/Watermelon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AlgoritmosCsharp.CodeForces
+{
+    class Watermelon
+    {
+        static void Main(string[] args)
+        {
+            byte w;
+
+            w = byte.Parse(Console.ReadLine());
+
+            if (w%2 == 0 && w > 2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
