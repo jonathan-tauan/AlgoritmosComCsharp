4c72a1c7a99ad38d11f862ab9d7c6d464b9daf51
Jonathan Maia <jtauan10@gmail.com>
2021-10-31 16:42:18 -0300

    CodeForces : Team.cs

---
+++ AlgoritmosCsharp/CodeForces/Team.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AlgoritmosCsharp.CodeForces
+{
+    class Team
+    {
+        static void Main(string[] args)
+        {
+            int problems, count=0, finalCount = 0;
+
+            problems = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < problems; i++)
+            {
+                string[] know = Console.ReadLine().Split();
+
+                for (byte j = 0; j < 3; j++)
+                {
+                    if (_ = byte.Parse(know[j]) == 1)
+                    {
+                        count++;                      
+                    }
+                }
+
+                if (count >= 2)
+                {
+                    finalCount++;
+                }
+                count = 0;
+            }
+            Console.WriteLine(finalCount);
+        }
+    }
+}
