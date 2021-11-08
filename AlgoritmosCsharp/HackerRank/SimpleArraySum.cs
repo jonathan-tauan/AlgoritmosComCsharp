3ed6ff2baf2af2f60ef80d50fd8719f402c153f1
Jonathan Maia <jtauan10@gmail.com>
2021-11-07 21:57:26 -0300

    Create SimpleArraySum.cs

---
+++ AlgoritmosCsharp/HackerRank/SimpleArraySum.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace AlgoritmosCsharp.HackerRank
+{
+
+    class Result
+    {
+        /*
+         * Complete the 'simpleArraySum' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY ar as parameter.
+         */
+
+        public static int simpleArraySum(List<int> ar)
+        {
+            int sum = 0;
+
+            foreach (int i in ar)
+            {
+                sum += i;
+            }
+
+            return sum;
+
+        }
+
+    }
+
+    class SimpleArraySum
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), false);
+
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+            int result = Result.simpleArraySum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
