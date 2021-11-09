abe89b95816a86742bd12a868553ac5500ab479e
Jonathan Maia <jtauan10@gmail.com>
2021-11-08 22:09:05 -0300

    Update SimpleArraySum.cs

--- AlgoritmosCsharp/HackerRank/SimpleArraySum.cs
+++ AlgoritmosCsharp/HackerRank/SimpleArraySum.cs
@@ -7,13 +7,6 @@ namespace AlgoritmosCsharp.HackerRank
 {
     class Result
     {
-        /*
-         * Complete the 'simpleArraySum' function below.
-         *
-         * The function is expected to return an INTEGER.
-         * The function accepts INTEGER_ARRAY ar as parameter.
-         */
-
         public static int simpleArraySum(List<int> ar)
         {
             int sum = 0;
