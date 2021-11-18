9f4586dc59fdce5cd1f29bf1df4f963850d6a804
Jonathan Maia <jtauan10@gmail.com>
2021-11-18 11:43:28 -0300

    Update CompareTheTriplets.cs

--- AlgoritmosCsharp/HackerRank/CompareTheTriplets.cs
+++ AlgoritmosCsharp/HackerRank/CompareTheTriplets.cs
@@ -6,16 +6,6 @@ namespace AlgoritmosCsharp.HackerRank
 {
     class ResultCompare
     {
-
-        /*
-         * Complete the 'compareTriplets' function below.
-         *
-         * The function is expected to return an INTEGER_ARRAY.
-         * The function accepts following parameters:
-         *  1. INTEGER_ARRAY a
-         *  2. INTEGER_ARRAY b
-         */
-
         public static List<int> compareTriplets(List<int> a, List<int> b)
         {
             List<int> result = new List<int>{ 0, 0 };
